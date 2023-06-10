d758d4d22e280e1f5d8ce85c33ce13cbd3fa8557
Timm Plagge <timm.plagge@tum.de>
2023-06-10 18:18:52 +0200

    Same setup as before but this time using the Swish function and for 100 epochs to see limit of the model. Got a score of 55.71

--- exercise_07/exercise_code/MyPytorchModel.py
+++ exercise_07/exercise_code/MyPytorchModel.py
@@ -36,15 +36,15 @@ class MyPytorchModel(nn.Module):
 
         self.model = nn.Sequential(
             nn.Linear(self.hparams["input_size"], self.hparams["n_hidden"]),
-            nn.Dropout(0.3),
+            nn.Dropout(self.hparams["input_dropout"]),
 
             nn.Linear(self.hparams["n_hidden"], self.hparams["n_hidden"]),
-            nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.SiLU(),
+            nn.Dropout(self.hparams["hidden_dropout"]),
 
             nn.Linear(self.hparams["n_hidden"], self.hparams["n_hidden"]),
-            nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.SiLU(),
+            nn.Dropout(self.hparams["hidden_dropout"]),
 
             nn.Linear(self.hparams["n_hidden"], self.hparams['num_classes'])
         )
